920030221f018d2b7c4a1b7209f95c58f811c0f9
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-04-15 17:36:40 +0300

    Feat: Implement GIF API Fetch Function
    The GIFs will be displayed on the cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
+import { useEffect } from 'react';
 import './App.css'
 
-async function App() {
+async function fetchGifs() {
+  const apiKey = '7uCiKGp7r7hEKsspvlhqflcCvrQHKFis';
+  const queryUrl = `https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=cats`;
+  try {
+    const response = await fetch(queryUrl, {mode: 'cors'});
+    if (!response.ok) throw new Error(`Failed to fetch with a wrong status`)
+    const imageData = await response.json();
+    const url = imageData.data.images.original.url;
+    if (!url) throw new Error(`No GIF url returned`)
+    return url;
+  }
+  catch(err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+function App() {
+  useEffect(() => {
+    (async () => {
+      let url = await fetchGifs();
+      console.log(url);
+    })();
+  }, []);
   return (
     <div></div>
   )
