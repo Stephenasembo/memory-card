0f5c8bdd68fdac99b2af0fb47794c6936bbce477
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-04-18 09:43:45 +0300

    Feat: Create Characters For Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,58 @@
 import { useEffect } from 'react';
 import './App.css'
 
+// These characters will have a name and image
+const characters = [
+  {
+    name: 'Ichigo Kurosaki',
+    id: 0,
+  },
+  {
+    name: 'Aizen Sosuke',
+    id: 1,
+  },
+  {
+    name: 'Inoue Orihime',
+    id: 2,
+  },
+  {
+    name: 'Rukia Kuchiki',
+    id: 3,
+  },
+  {
+    name: 'Byakuya Kuchiki',
+    id: 4,
+  },
+  {
+    name: 'Chad Yasutora',
+    id: 5,
+  },
+  {
+    name: 'Yoruichi Shihouin',
+    id: 6,
+  },
+  {
+    name: 'Tier Halibel',
+    id: 7,
+  },
+  {
+    name: 'Uryu Ishida',
+    id: 8,
+  },
+  {
+    name: 'Kisuke Urahara',
+    id: 9,
+  },
+  {
+    name: 'Rangiku Matsumoto',
+    id: 10,
+  },
+  {
+    name: 'Retsu Unohana',
+    id: 11,
+  },
+]
+
 async function fetchGifs() {
   const apiKey = '7uCiKGp7r7hEKsspvlhqflcCvrQHKFis';
   const queryUrl = `https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=cats`;
